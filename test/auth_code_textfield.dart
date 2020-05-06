fa6756e6fcc1838c9feef43a53e715ea8f7e03c0
JerryFans <fanjiarong_haohao@163.com>
2020-05-06 11:06:23 +0800

    config

--- test/auth_code_textfield.dart
+++ test/auth_code_textfield.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_auth_code_textfield/auth_code_textfield.dart';
+import 'package:auth_code_textfield/auth_code_textfield.dart';
 
 void main() {
   
